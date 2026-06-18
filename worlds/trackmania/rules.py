2bb003a60c1607b4f3879a75b47b028cfad6cb56
Merlin <yannsalaun27@gmail.com>
2026-06-18 22:44:34 +0200

    Correcting an error with the world.get_entrance

--- worlds/trackmania/rules.py
+++ worlds/trackmania/rules.py
@@ -18,49 +18,49 @@ def set_rules(world: "TrackmaniaWorld"):
     progression_system = world.options.progression_system.value
 
     if progression_system == 0 :
-        set_rule(world.multiworld.get_entrance("Victory!", world.player),
+        set_rule(world.get_entrance("Victory!"),
                  lambda state: state.has(get_progression_medal(world), world.player, final_medal_requirement))
 
     if progression_system == 1 : 
         progression_medal = get_progression_medal(world)
         if progression_medal == "Author Medal":
-            set_rule(world.get_entrance("Victory!", world.player),
+            set_rule(world.get_entrance("Victory!"),
                  lambda state: (state.count("Author Medal", world.player) +
                         state.count("Gold Medal", world.player) +
                         state.count("Silver Medal", world.player) +
                         state.count("Bronze Medal", world.player)) >= final_medal_requirement * 4)
         if progression_medal == "Gold Medal":
-            set_rule(world.get_entrance("Victory!", world.player),
+            set_rule(world.get_entrance("Victory!"),
                  lambda state: (state.count("Gold Medal", world.player) +
                         state.count("Silver Medal", world.player) +
                         state.count("Bronze Medal", world.player)) >= final_medal_requirement * 3)
         if progression_medal == "Silver Medal":
-            set_rule(world.get_entrance("Victory!", world.player),
+            set_rule(world.get_entrance("Victory!"),
                  lambda state: (state.count("Silver Medal", world.player) +
                         state.count("Bronze Medal", world.player)) >= final_medal_requirement * 2)
         if progression_medal == "Bronze Medal":
-            set_rule(world.get_entrance("Victory!", world.player),
+            set_rule(world.get_entrance("Victory!"),
                  lambda state: (state.count("Bronze Medal", world.player)) >= final_medal_requirement)
             
     if progression_system == 2 :
         progression_medal = get_progression_medal(world)
         if progression_medal == "Author Medal":
-            set_rule(world.get_entrance("Victory!", world.player),
+            set_rule(world.get_entrance("Victory!"),
                  lambda state: (state.count("Author Medal", world.player) * 5 +
                      state.count("Gold Medal", world.player) * 3 +
                      state.count("Silver Medal", world.player) * 1 +
                      state.count("Bronze Medal", world.player) * 1) >= final_medal_requirement * 10)
         if progression_medal == "Gold Medal":
-            set_rule(world.get_entrance("Victory!", world.player),
+            set_rule(world.get_entrance("Victory!"),
                  lambda state: (state.count("Gold Medal", world.player) * 5 +
                         state.count("Silver Medal", world.player) * 3 +
                         state.count("Bronze Medal", world.player) * 2) >= final_medal_requirement * 10)
         if progression_medal == "Silver Medal":
-            set_rule(world.get_entrance("Victory!", world.player),
+            set_rule(world.get_entrance("Victory!"),
                  lambda state: (state.count("Silver Medal", world.player) * 7 +
                         state.count("Bronze Medal", world.player) * 3) >= final_medal_requirement * 10)
         if progression_medal == "Bronze Medal":
-            set_rule(world.get_entrance("Victory!", world.player),
+            set_rule(world.get_entrance("Victory!"),
                  lambda state: (state.count("Bronze Medal", world.player) * 10) >= final_medal_requirement * 10)
 
     # default just in case
